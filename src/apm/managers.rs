a89ea147c256bc2d388a350888c77263b954e735
james durban <james_d02@protonmail.com>
2021-07-20 23:29:43 +0100

    changes

--- src/apm/managers.rs
+++ src/apm/managers.rs
@@ -11,3 +11,18 @@ pub fn choco() -> PackageManager { return choco::get_choco_manager(); }
 pub fn dnf() -> PackageManager { return dnf::get_dnf_manager(); }
 pub fn pacman() -> PackageManager { return pacman::get_pacman_manager(); }
 pub fn yum() -> PackageManager { return yum::get_yum_manager(); }
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_apt() {
+        let apt: PackageManager = apt();
+        assert_eq!(apt.name, "Aptitude Package Manager");
+        assert_eq!(apt.package_name, "apt-get");
+    }
+
+}
